dc62f7b62d4514b43d23859047e3887bfb0259f3
Istheaksayem <istheakahmed9345@gmail.com>
2025-09-12 18:30:33 +0600

    recap card

--- script/index.js
+++ script/index.js
@@ -12,7 +12,15 @@ const loadLevelWord = (id) => {
 }
 const displayLevelWord = (words) => {
     const wordContainer = document.getElementById("word-container")
-    // wordContainer.innerHTML = "";
+    wordContainer.innerHTML = "";
+
+//     {
+//     "id": 99,
+//     "level": 2,
+//     "word": "Kind",
+//     "meaning": "দয়ালু",
+//     "pronunciation": "কাইন্ড"
+// }
 
     words.forEach(word => {
 
@@ -21,9 +29,9 @@ const displayLevelWord = (words) => {
         const card = document.createElement("div")
         card.innerHTML=`
            <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-        <h2 class="font-bold text-xl">Eager</h2>
+        <h2 class="font-bold text-xl">${word.word}</h2>
         <p class="font-semibold">Meaning /Pronounciation</p>
-        <div class="text-2xl font-medium font-bangla">"আগ্রহী / ইগার"</div>
+        <div class="text-2xl font-medium font-bangla">${word.meaning}/ ${word.pronunciation}"</div>
         <div class="flex justify-between items-center">
             <button class="btn bg-[#1A91ff10] hover:bg-[#1A91ff80]"><i class="fa-solid fa-circle-info"></i></button>
             <button class="btn bg-[#1A91ff10] hover:bg-[#1A91ff80]"><i class="fa-solid fa-volume-high"></i></button>
